c546d4714d7c69261ed6b515b7b4ef18a9b44fb9
ele-dar <104769631+ele-dar@users.noreply.github.com>
2022-09-09 20:39:19 +0300

    provisional styles and script added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import logo from './images/logo.svg'
-import iconDollar from './images/icon-dollar.svg'
-import iconPerson from './images/icon-person.svg'
-
 
 function App() {
+  const [form, setForm] = useState({
+    bill: '',
+    tip: '',
+    people: ''
+  })
+  const [showCustom, setShowCustom] = useState(false)
+  const [tip, setTip] = useState(null)
+  const [total, setTotal] = useState(null)
+
+  const calculateTip = () => (+form.tip * +form.bill / 100) / +form.people
+  const calculateTotal = () => (+form.bill + (+form.tip * +form.bill / 100)) / +form.people
+
+  useEffect(() => {
+    if (form.bill && form.tip && form.people) {
+      setTip(calculateTip())
+      setTotal(calculateTotal())
+    }
+  }, [form])
+
+  const handleInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleRadioInput = (e) => {
+    setShowCustom(false)
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleReset = () => {
+    setForm({
+      bill: '',
+      tip: '',
+      people: ''
+    })
+  }
+
   return (
     <>
       <header>
@@ -13,55 +47,75 @@ function App() {
 
       <main>
         <div className="container">
-          <div className="form-container">
-            <form>
-              <div>
-                <h4>Bill</h4>
-                <div><img src={iconDollar} alt="Dollar icon" /><input type="number" placeholder="0" /></div>
-              </div>
-              <div>
-                <h4 for="">Select Tip %</h4>
-                <div className="tip-switch-field">
-                  <input type="radio" id="tip-5" name="tip" value="5" />
-                  <label for="tip-5">5</label>
-                  <input type="radio" id="tip-10" name="tip" value="10" />
-                  <label for="tip-10">10</label>
-                  <input type="radio" id="tip-15" name="tip" value="15" />
-                  <label for="tip-15">15</label>
-                  <input type="radio" id="tip-25" name="tip" value="25" />
-                  <label for="tip-25">25</label>
-                  <input type="radio" id="tip-50" name="tip" value="50" />
-                  <label for="tip-50">50</label>
-                  <input type="radio" id="tip-custom-radio" name="tip" value="custom" />
-                  <label for="tip-custom-radio">Custom</label>
-                  <input type="text" id="tip-custom-text" name="tip" value="0" />
-                </div>
-              </div>
-              <div>
-                <h4>Number of People</h4>
-                <div><img src={iconPerson} alt="Person icon" /><input type="number" placeholder="0" /></div>
-              </div>
-            </form>
-          </div>
-          <div className="output-container">
+          <form>
             <div>
-              <h4>Tip Amount <span>/ person</span></h4>
-              <div>$ 0.00</div>
+              <h4>Bill</h4>
+              <input id='bill-input' type="number" name='bill' placeholder="0" onChange={handleInput} value={form.bill} />
             </div>
             <div>
-              <h4>Total <span>/ person</span></h4>
-              <div>$ 0.00</div>
-            </div>
-            <button>Reset</button>
-          </div>
+              <h4>Select Tip %</h4>
+              <div className="tip-switch-field">
+                <label >
+                  <input type="radio" id="tip-5" name="tip" value="5" onChange={handleRadioInput} />
+                  <span>5%</span>
+                </label>
+                <label>
+                  <input type="radio" id="tip-10" name="tip" value="10" onChange={handleRadioInput} />
+                  <span>10%</span>
+                </label>
+                <label>
+                  <input type="radio" id="tip-15" name="tip" value="15" onChange={handleRadioInput} />
+                  <span>15%</span>
+                </label>
+                <label>
+                  <input type="radio" id="tip-25" name="tip" value="25" onChange={handleRadioInput} />
+                  <span>25%</span>
+                </label>
+                <label>
+                  <input type="radio" id="tip-50" name="tip" value="50" onChange={handleRadioInput} />
+                  <span>50%</span>
+                </label>
 
+                {showCustom ? (
+                  <label>
+                    <input type="radio" id="custom-radio" name="tip" />
+                    <input type="number" id="custom-input" name="tip" placeholder='0' autoFocus onChange={handleInput} value={form.tip} />
+                  </label>
+                ) : (
+                  <label>
+                    <input type="radio" id="custom-radio" name="tip" onClick={() => setShowCustom(true)} />
+                    <span>Custom</span>
+                  </label>
+                )}
+
+              </div>
+            </div>
+            <div>
+              <h4>Number of People</h4>
+              <input id='people-input' type="number" name='people' placeholder="0" onChange={handleInput} value={form.people} />
+            </div>
+          </form>
+
+          <div className="green-container">
+            <div className='output-container'>
+              <div>
+                <h4>Tip Amount <span>/ person</span></h4>
+                {tip ? <div className='output-amount'>${tip.toFixed(2)}</div> : <div className='output-amount'>$0.00</div>}
+              </div>
+              <div>
+                <h4>Total <span>/ person</span></h4>
+                {total ? <div className='output-amount'>${total.toFixed(2)}</div> : <div className='output-amount'>$0.00</div>}
+              </div>
+            </div>
+            <button className='btn' onClick={handleReset}>Reset</button>
+          </div>
         </div>
+      </main >
 
-      </main>
       <footer>
-        <div class="attribution">
-          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-          Coded by <a href="#">ele-dar</a>.
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+          Coded by <a href="https://github.com/ele-dar">ele-dar</a>.
         </div>
       </footer>
 
